e3df78415ea0b09eb5fcfa1745a1328c8a41e978
raghuindra <raghuindra.p@gmail.com>
2021-01-21 16:57:40 -0600

    task one fix and updates

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   addToReadingList,
@@ -9,52 +9,63 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
-  styleUrls: ['./book-search.component.scss']
+  styleUrls: ['./book-search.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookSearchComponent implements OnInit {
   books: ReadingListBook[];
 
-  searchForm = this.fb.group({
-    term: ''
-  });
+  // searchForm = this.fb.group({
+  //   term: ''
+  // });
 
   constructor(
     private readonly store: Store,
-    private readonly fb: FormBuilder
+    private metaTagService: Meta,
+    private cd: ChangeDetectorRef
   ) {}
 
-  get searchTerm(): string {
-    return this.searchForm.value.term;
-  }
+  // get searchTerm(): string {
+  //   return this.searchForm.value.term;
+  // }
 
   ngOnInit(): void {
     this.store.select(getAllBooks).subscribe(books => {
       this.books = books;
+      this.cd.markForCheck();
     });
+    this.metaTagService.addTags([
+      { name: 'keywords', content: 'Angular SEO Integration, Read Books, Search Books' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'T-Mobile Web UI Search' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2021-01-21', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' }
+    ]);
   }
 
-  formatDate(date: void | string) {
-    return date
-      ? new Intl.DateTimeFormat('en-US').format(new Date(date))
-      : undefined;
-  }
+  // formatDate(date: void | string) {
+  //   return date
+  //     ? new Intl.DateTimeFormat('en-US').format(new Date(date))
+  //     : undefined;
+  // }
 
-  addBookToReadingList(book: Book) {
+  onAddedBookToReadingList(book: Book) {
     this.store.dispatch(addToReadingList({ book }));
   }
 
-  searchExample() {
-    this.searchForm.controls.term.setValue('javascript');
-    this.searchBooks();
+  searchExample(searchTerm : string) {
+    this.onSearchBooks(searchTerm);
   }
 
-  searchBooks() {
-    if (this.searchForm.value.term) {
-      this.store.dispatch(searchBooks({ term: this.searchTerm }));
+  onSearchBooks(searchTerm: string) {
+    if (searchTerm) {
+      this.store.dispatch(searchBooks({ term: searchTerm }));
     } else {
       this.store.dispatch(clearSearch());
     }
